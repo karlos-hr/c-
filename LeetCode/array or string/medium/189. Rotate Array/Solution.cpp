816084d794156ed7f3047de2bcf748c14e14f242
carlosh-rios <hortarios.carlos@gmail.com>
2024-08-31 14:51:33 +0100

    feat: LeetCode - 189. Rotate Array

--- LeetCode/array or string/medium/189. Rotate Array/Solution.cpp
+++ LeetCode/array or string/medium/189. Rotate Array/Solution.cpp
@@ -5,8 +5,23 @@ using namespace std;
 
 class Solution {
 public:
+
     void rotate(vector<int>& nums, int k) {
-        
+        int size = nums.size();
+        k %= size; // 3
+        reverse(nums,0, size - 1); // 7, 6, 5, 4, 3, 2, 1
+        reverse(nums,0, k - 1); // 5, 6, 7, 4, 3, 2, 1
+        reverse(nums,k, size - 1); // 5, 6, 7, 1, 2, 3, 4
+    }
+
+    void reverse (vector<int>& nums, int left, int right ) { 
+        while(left < right) {
+            int aux = nums[left];
+            nums[left] = nums[right];
+            nums[right] = aux;
+            ++left;
+            --right;
+        }
     }
 
     static void print(vector<int> nums){
@@ -24,7 +39,7 @@ int main() {
     Solution sol;
 
     vector<int> nums = {1,2,3,4,5,6,7};
-    int k = 3;
+    int k = 2;
     sol.rotate(nums,k);
     Solution::print(nums);
     
